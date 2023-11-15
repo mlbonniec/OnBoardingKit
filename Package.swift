b85dcf5d900408e1cc052387cf7828c0e086e13b
Mathis Le Bonniec <mathislbonniec@gmail.com>
2023-11-15 14:18:05 +0100

    refactor(Package): remove tests target

--- Package.swift
+++ Package.swift
@@ -15,7 +15,6 @@ let package = Package(
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
-    .target(name: "OnBoarding"),
-    .testTarget(name: "OnBoardingTests", dependencies: ["OnBoarding"])
+    .target(name: "OnBoarding")
   ]
 )
