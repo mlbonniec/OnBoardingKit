0305fe68aece34e346792bbf8acd57edfafd5fcc
Mathis Le Bonniec <mathislbonniec@gmail.com>
2023-11-15 15:05:38 +0100

    feat(feature): create OnBoarding Feature view

---
+++ Sources/OnBoardingKit/Components/OnBoardingFeatureView.swift
@@ -0,0 +1,61 @@
+//
+//  OnBoardingFeatureView.swift
+//
+//
+//  Created by Mathis Le Bonniec on 13/11/2023.
+//
+
+import SwiftUI
+
+struct OnBoardingFeatureView: View {
+  var image: Image?
+  var label: Text?
+  var description: Text
+
+  var body: some View {
+    HStack(spacing: Constants.Spacings.small) {
+      image?
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 35, height: 35)
+        .foregroundStyle(Color.accentColor)
+
+      VStack(alignment: .leading, spacing: 3) {
+        label?
+          .fontWeight(.semibold)
+
+        description
+          .foregroundStyle(.gray)
+      }
+      .font(.system(size: 16))
+      .lineSpacing(3)
+
+      Spacer(minLength: 0)
+    }
+    .frame(maxWidth: .infinity)
+  }
+}
+
+#Preview("Description Only") {
+  OnBoardingFeatureView(
+    description: Text("Suggestions only appear if you write about them or save them to your journal.")
+  )
+  .padding()
+}
+
+#Preview("Image + Description") {
+  OnBoardingFeatureView(
+    image: Image(systemName: "lightbulb.max.fill"),
+    description: Text("Suggestions only appear if you write about them or save them to your journal.")
+  )
+  .padding()
+}
+
+#Preview("Image + Label + Description") {
+  OnBoardingFeatureView(
+    image: Image(systemName: "lightbulb.max.fill"),
+    label: Text("Suggestions Are Private"),
+    description: Text("Suggestions only appear if you write about them or save them to your journal.")
+  )
+  .padding()
+}
