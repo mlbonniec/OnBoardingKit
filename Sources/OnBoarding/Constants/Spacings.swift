82ff9acd49a30374f31a4bf6236a1d7435ada77e
Mathis Le Bonniec <mathislbonniec@gmail.com>
2023-11-15 14:18:47 +0100

    feat(constants): extends constants for spacing

---
+++ Sources/OnBoarding/Constants/Spacings.swift
@@ -0,0 +1,24 @@
+//
+//  Spacings.swift
+//
+//
+//  Created by Mathis Le Bonniec on 14/11/2023.
+//
+
+import Foundation
+
+extension Constants {
+  enum Spacings {
+    /// 6
+    static let tiny: CGFloat = 6
+
+    /// 12
+    static let small: CGFloat = 12
+
+    /// 18
+    static let medium: CGFloat = 18
+
+    /// 24
+    static let large: CGFloat = 24
+  }
+}
