bfcff2542853c1facd16b7324b4af1dd6c3d14c9
Mathis Le Bonniec <mathislbonniec@gmail.com>
2023-11-15 15:05:19 +0100

    feat(header): create OnBoarding Header view

---
+++ Sources/OnBoardingKit/Components/OnBoardingHeaderView.swift
@@ -0,0 +1,54 @@
+//
+//  OnBoardingHeaderView.swift
+//
+//
+//  Created by Mathis Le Bonniec on 13/11/2023.
+//
+
+import SwiftUI
+
+struct OnBoardingHeaderView: View {
+  var image: Image?
+  var title: Text
+  var description: Text?
+
+  var body: some View {
+    VStack(alignment: .center, spacing: Constants.Spacings.medium) {
+      image?
+        .resizable()
+        .frame(width: 75, height: 75)
+
+      title
+        .font(.largeTitle)
+        .bold()
+        .multilineTextAlignment(.center)
+        .fixedSize()
+
+      description?
+        .font(.callout)
+        .multilineTextAlignment(.center)
+        .foregroundStyle(.gray)
+    }
+  }
+}
+
+#Preview("Title Only") {
+  OnBoardingHeaderView(
+    title: Text("First steps\nwith OnBoarding")
+  )
+}
+
+#Preview("Image + Title") {
+  OnBoardingHeaderView(
+    image: Image(systemName: "square.fill"),
+    title: Text("First steps\nwith OnBoarding")
+  )
+}
+
+#Preview("Image + Title + Description") {
+  OnBoardingHeaderView(
+    image: Image(systemName: "square.fill"),
+    title: Text("First steps\nwith OnBoarding"),
+    description: Text("Discover how to use this awesome Apple like on boarding view library")
+  )
+}
