e45bd0271c1ff34ca074e322370f4b466ebd64da
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-01-15 15:42:04 +0100

    feat(presentation): create an enum with app storage keys

---
+++ Sources/OnBoardingKit/Models/OnBoardingPresentationModel.swift
@@ -0,0 +1,10 @@
+//
+//  OnBoardingPresentationModel.swift
+//  
+//
+//  Created by Mathis Le Bonniec on 15/01/2024.
+//
+
+enum OnBoardingAppStorage: String {
+  case hasOnBoardingBeenPresented
+}
